bda08dc5000b053795c6c68ce7670706b4220e2d
byzg <byzg00@gmail.com>
2018-09-11 02:26:51 +0300

    wish update

--- app/controllers/api/wishes_controller.rb
+++ app/controllers/api/wishes_controller.rb
@@ -2,16 +2,26 @@ class Api::WishesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    render json: current_user.wishes
+    render json: collection
   end
 
   def create
-    wish = current_user.wishes.create(wish_params)
+    wish = collection.create(wish_params)
+    render json: wish
+  end
+
+  def update
+    wish = collection.find(params[:id])
+    wish.update(wish_params)
     render json: wish
   end
 
   private
   def wish_params
     params.require(:wish).permit(:title, :text, :price)
+  end
+
+  def collection
+    current_user.wishes
   end
 end
