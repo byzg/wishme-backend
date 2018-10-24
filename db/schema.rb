dd37acd92427a103492f934d83114fe89953268e
byzg <byzg00@gmail.com>
2018-10-25 02:24:46 +0300

    ChangePriceType migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180829212129) do
+ActiveRecord::Schema.define(version: 20181024231241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20180829212129) do
   create_table "wishes", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "price"
+    t.money "price", scale: 2
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
