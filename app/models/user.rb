2804dd0c2c995366130739b3ce341d40c408c140
byzg <byzg00@gmail.com>
2018-12-03 01:17:50 +0300

    use user_serializer for login API

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :wishes, dependent: :destroy
+
+  def token_validation_response
+    UserSerializer.new(self).attributes
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < BaseSerializer
   attributes :id, :name, :wishes_count
+
+  def wishes_count
+    object.try(:wishes_count) || object.wishes.count
+  end
 end
